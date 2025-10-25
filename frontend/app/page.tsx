4201eab6889b9523cb6e055f337048d62fd67d0a
Teerth Kulkarni <teerth.kulkarni@walchandsangli.ac.in>
2025-10-25 11:32:55 +0530

    soon landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/dist/client/link";
+import Dashboard from "./(main)/Dashboard/page";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      hi
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className="">Landing Page</h1>
+        <br />
+        <Link href="/Dashboard">Go to Dashboard</Link>
+      </div>
     </>
   );
 }
